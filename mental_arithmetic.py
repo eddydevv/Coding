3bdddae781616e345bfa092aead19a988e33dfcf
Edwin Agbenyega <eddybank03@gmail.com>
2017-01-05 16:19:47 -0700

    Beta 0.1 with individual graphs and such

--- mental_arithmetic.py
+++ mental_arithmetic.py
@@ -42,14 +42,21 @@ def record_count(r, p):
     info[p]['rand_nums'].append(str(r))
     info[p]['date_time'].append(str(datetime.datetime.now()))
     print(info)
+    file(p)
 
 
-def file_path(p, file_type):
+def file_path(p, file_type, doc_type):
     path = "./test_results/"
-    if not os.path.exists(path):
-        os.makedirs(path)
+    timed_path = "./test_results/timestamped/"
+    if not os.path.exists(timed_path):
+        os.makedirs(timed_path)
 
-    file_name = p + str(datetime.datetime.now()) + file_type
+    if doc_type == 'summary':
+        file_name  = p + file_type
+    elif doc_type == 'timestamped':
+        file_name = p + str(datetime.datetime.now()) + file_type
+        path = timed_path
+
     complete_name = os.path.join(path ,file_name)
     return complete_name
 
@@ -58,8 +65,24 @@ def plot_graph():
     for p in participants:
         plt.plot(info[p]['rand_nums'], info[p]['count'], 'ro')
         plt.xlim(0,18)
-        plt.ylim(0,110)
-        plt.savefig(file_path(p, ".png"))
+        plt.ylim(0,float(max(info[p]['count']))+1)
+        plt.savefig(file_path(p, ".png", "timestamped"))
+
+        with open(file_path(p, ".txt", "summary")) as f:
+            lines = f.readlines()
+
+        x, y = [], []
+        for line in lines[6:]:
+            line = line.split()
+            x.append(int(line[0]))
+            y.append(float(line[1]))
+
+        plt.xlim(0,18)
+        plt.ylim(0,120)
+        plt.plot(x,y,'ro')
+        print(x,y)
+        plt.savefig(file_path(p, ".png", "summary"))
+        plt.close()
 
 
 def show_info():
@@ -73,21 +96,29 @@ def show_info():
         for i in range(len(info[p]['count'])):
             print("\t" + info[p]['rand_nums'][i] + "\t\t" + info[p]['count'][i])
         print("-" * 50)
-        file(p)
 
 
 def file(p):
-    f = open(file_path(p, ".txt"), 'w')
-    f.write("-" * 80 + "\n\n")
-    f.write(" "* 30 + p + "\n")
-    f.write("-" *80 + "\n")
-    f.write("  Random Number          Count                          DateTime\n")
-    f.write("-----------------      ---------      -----------------------------------------\n")
-    for i in range(len(info[p]['count'])):
-        f.write("\t" + info[p]['rand_nums'][i] + "\t\t" + info[p]['count'][i] + "\t\t" + info[p]['date_time'][i] + "\n")
-    f.write("-" * 80 + "\n")
-    f.write("\n")
-    f.close()
+    tpath = file_path(p, ".txt", "timestamped")
+    spath = file_path(p, ".txt", "summary")
+    timestamped_file = {'path': tpath, 'file': open(tpath, 'w')}
+    summary_file = {'path': spath, 'file': open(spath, 'a')}
+    for f in [timestamped_file, summary_file]:
+        if(os.path.getsize(f['path']) == 0):
+            f['file'].write("-" * 80 + "\n\n")
+            f['file'].write(" "* 30 + p + "\n")
+            f['file'].write("-" *80 + "\n")
+            f['file'].write("  Random Number          Count                          DateTime\n")
+            f['file'].write("-----------------      ---------      -----------------------------------------\n")
+            for i in range(len(info[p]['count'])):
+                f['file'].write("\t" + info[p]['rand_nums'][i] + "\t\t" + info[p]['count'][i] + "\t\t" + info[p]['date_time'][i] + "\n")
+        else:
+            f['file'].write("\t" + info[p]['rand_nums'][-1] + "\t\t" + info[p]['count'][-1] + "\t\t" + info[p]['date_time'][-1] + "\n")
+
+        if(f['file'] == timestamped_file):
+            f['file'].write("-" * 80 + "\n")
+            f['file'].write("\n")
+        f['file'].close()
 
 
 def enter_name():
@@ -108,8 +139,8 @@ while(options != '0'):
     if(options == '1'):
         r = generate_rand()
         record_count(r, cp)
-    elif(options == '2'):
         plot_graph()
+    elif(options == '2'):
         show_info()
     elif(options == '3'):
         cp = enter_name()
